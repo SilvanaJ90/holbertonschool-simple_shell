767ef199c92798bc67f09a30a7745b8fa9e5e9c8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-08-17 19:55:21 -0500

    update

--- builtin.c
+++ builtin.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _builtin -check code
+ * @args: value
+ */
+
+/**
+ * struct - check code
+ * @builtins: value
+ */
+
 void _builtin(char **args)
 {
 	int numBuiltin = 0;
@@ -24,43 +34,63 @@ void _builtin(char **args)
 	}
 }
 
+/**
+ * _eexit - check code
+ * @args: value
+ */
+
 void _eexit(char **args)
 {
 	(void)args;
-		exit(0);
+	exit(0);
 }
 
-void _cd(char **args) {
-    if (args[1] == NULL) {
-        fprintf(stderr, "hsh: cd: missing argument\n");
-    } else {
-        if (chdir(args[1]) != 0) {
-            perror("hsh: cd");
-        }
-    }
+/**
+ * _cd -check code
+ * @args: value
+ */
+
+void _cd(char **args)
+{
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "hsh: cd: missing argument\n");
+	} else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("hsh: cd");
+		}
+	}
 }
 
-void _help(char **args) {
-    char *helptext =
-        "The following commands are available:\n"
-        "  cd       Change the working directory.\n"
-        "  exit     Exit the shell.\n"
-        "  help     Print this help text.\n";
-    printf("%s", helptext);
 
-    (void)args;
+/**
+ * _help -check code
+ * @args: value
+ */
+
+void _help(char **args)
+{
+	char *helptext =
+		"The following commands are available:\n"
+		"  cd       Change the working directory.\n"
+		"  exit     Exit the shell.\n"
+		"  help     Print this help text.\n";
+	printf("%s", helptext);
+	(void)args;
 }
+
+/**
+ * _env -check code
+ * @args: value
+ */
 
 void _env(char **args)
 {
-    char **env = environ;
+	char **env = environ;
 
-    /*for (; *s; s++) {
-        printf("%s\n", *s);
-    };*/
-    while (*env)
-      printf("%s\n", *env++);
-
-
-    (void)args;
+	while (*env)
+		printf("%s\n", *env++);
+	(void)args;
 }
