718a77d0c59666b85970a45efc4530a693a99c8b
giogap <giogap@P-GIOVANNI.localdomain>
2022-08-17 23:42:21 -0500

    update builtin

--- builtin.c
+++ builtin.c
@@ -1,5 +1,4 @@
-#
-include "main.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -13,7 +12,6 @@ void _builtin(char **args)
 {
 	int numBuiltin = 0;
 	int i;
-<<<<<<< HEAD
 
 	  struct builtin builtins[] = {
         {"exit",_eexit},
@@ -23,17 +21,6 @@ void _builtin(char **args)
     };
 
 
-=======
-/**
- * struct builtin - check code
- */
-	struct builtin builtins[] = {
-			{"exit", _eexit},
-			{"cd", _cd},
-			{"help", _help},
-			{"env", _env},
-	};
->>>>>>> 6dd5842 (update-betty)
 	numBuiltin = sizeof(builtins) / sizeof(struct builtin);
 
 	for (i = 0; i < numBuiltin; i++)
@@ -75,6 +62,101 @@ void _cd(char **args)
 		}
 	}
 }
+
+
+/**
+ * _help -check code
+ * @args: value
+ */
+
+void _help(char **args)
+{
+	char *helptext =
+		"The following commands are available:\n"
+		"  cd       Change the working directory.\n"
+		"  exit     Exit the shell.\n"
+		"  help     Print this help text.\n";
+	printf("%s", helptext);
+	(void)args;
+}
+
+/**
+ * _env -check code
+ * @args: value
+ */
+
+void _env(char **args)
+{
+	char **env = environ;
+
+	while (*env)
+		printf("%s\n", *env++);
+	(void)args;
+}#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _builtin -check code
+ * @args: value
+ */
+
+
+
+void _builtin(char **args)
+{
+	int numBuiltin = 0;
+	int i;
+
+	  struct builtin builtins[] = {
+        {"exit",_eexit},
+        {"cd", _cd},
+        {"help", _help},
+        {"env", _env},
+    };
+
+
+	numBuiltin = sizeof(builtins) / sizeof(struct builtin);
+
+	for (i = 0; i < numBuiltin; i++)
+	{
+		if (_strcmp(args[0], builtins[i].name) == 0)
+		{
+			builtins[i].func(args);
+			return;
+		}
+	}
+}
+
+/**
+ * _eexit - check code
+ * @args: value
+ */
+
+void _eexit(char **args)
+{
+	(void)args;
+	exit(0);
+}
+
+/**
+ * _cd -check code
+ * @args: value
+ */
+
+void _cd(char **args)
+{
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "hsh: cd: missing argument\n");
+	} else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("hsh: cd");
+		}
+	}
+}
+
 
 /**
  * _help -check code
